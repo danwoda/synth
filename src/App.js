3c4c9c62f30858935e761be3144076eb7f447d52
danwoda <dcwoda@gmail.com>
2023-11-08 16:17:32 -0500

    added 12 key piano

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Panel from './components/Panel';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
     <div className="App">
-      <Panel/>
+      <Keyboard/>
     </div>
   );
 }
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,35 +1,11 @@
-export default function Key({ frequency, waveformType }) {
-  let ctx = new AudioContext()
-  let osc = false
-
-  function clickHandler() {
-    console.log('key click')
-    console.log(waveformType, frequency)
-    if (!osc) {
-      osc = ctx.createOscillator()
-      osc.type = waveformType
-      osc.frequency.value = frequency
-      osc.connect(ctx.destination)
-      osc.start(ctx.currentTime)
-      osc.stop(ctx.currentTime + 2)
-      osc = false
-    }
-  }
-
-  //This isn't working very well. Should probably be handled page-wide and passed down to each key
-  function keyDownHandler(e) {
-    if (e.key === 'h') {
-      clickHandler()
-    }
-  }
-
-
+export default function Key({ onClick, noteName }) {
+  const keyColor = noteName.includes('b') ? 'black-key' : 'white-key';
 
   return (
-    <div className="white-key"
-      onClick={clickHandler} 
-      onKeyDown={keyDownHandler}
-      tabIndex={0}>
+    <div className={keyColor + ' key'}
+    onClick={onClick} 
+    >
+      {noteName.slice(1)}
     </div>
   )
 }
